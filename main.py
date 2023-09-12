2c02b2f700030a703c55a9825da49c136512fa3b
mark <wernsdorfer@gmail.com>
2023-09-12 08:26:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SpotTheBot',
+    version='0.01',
+    packages=[''],
+    url='spotthebot.app',
+    license='GNU AGPLv3',
+    author='mark',
+    author_email='wernsdorfer@gmail.com',
+    description='Learn to uncover AI generated texts'
+)
