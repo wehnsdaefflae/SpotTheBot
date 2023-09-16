1695c17543464da456b48fe0ed2cf84a73ea790b
mark <wernsdorfer@gmail.com>
2023-09-16 10:13:55 +0200

    pages set up

--- main.py
+++ main.py
@@ -9,44 +9,114 @@ def generate_name(label: Label) -> None:
     label.text = f"[name {random.randint(1, 100)}]"
 
 
+def create_footer() -> None:
+    with ui.footer() as footer:
+        with ui.row() as row:
+            label_aces = ui.link("check out top aces", "/aces")
+            label_results = ui.link("check out results", "/results")
+            label_about = ui.link("about", "/about")
+
+
 def log_in(user_name: str) -> None:
     app.storage.user["user_name"] = user_name
     ui.open("/")
 
 
-def start_game() -> None:
-    user_name = app.storage.user.get("user_name", None)
-    ui.label(f"Hello {user_name}!")
-
-
-def landing_page() -> None:
-    title_label = ui.label("Look out for")
-    title_label.classes("text-h4 font-bold text-grey-8")
-
-    for i in range(4):
-        each_indicator_label = ui.label(f"[indicator {i + 1}] [accuracy]")
-
-    button_start = ui.button("SPOT THE BOT")
-    label_as = ui.label("as")
-    label_name = ui.label()
-    generate_name(label_name)
-
-    label_name.on("click", lambda: generate_name(label_name))
-    button_start.on("click", lambda: log_in(label_name.text))
-
-    with ui.row() as row:
-        label_aces = ui.label("check out top aces")
-        label_results = ui.label("check out results")
-
-
 @ui.page("/")
 def index_page() -> None:
-    user_name = app.storage.user.get("user_name", None)
     with ui.column() as column:
-        if user_name is None:
-            landing_page()
-        else:
-            start_game()
+        title_label = ui.label("Look out for")
+        title_label.classes("text-h4 font-bold text-grey-8")
+
+        for i in range(4):
+            each_indicator_label = ui.label(f"[indicator {i + 1}] [accuracy]")
+
+        user_name = app.storage.user.get("user_name", None)
+        with ui.column() as column:
+            if user_name is None:
+                label_name = ui.label()
+                generate_name(label_name)
+                label_name.on("click", lambda: generate_name(label_name))
+
+                button_name = ui.button("Take name")
+                button_name.on("click", lambda: log_in(user_name))
+                user_name = label_name.text
+
+            else:
+                label_welcome = ui.label(f"Welcome {user_name}! Get ready to")
+                button_start = ui.button("SPOT THE BOT", on_click=lambda: ui.open("/game"))
+
+        create_footer()
+
+
+@ui.page("/game")
+def game_page() -> None:
+    user_name = app.storage.user.get("user_name", None)
+    if user_name is None:
+        ui.open("/")
+        return
+
+    with ui.column() as column:
+        text_context = ui.markdown("source information")
+        text_content = ui.markdown("""Die ADF-RDA ist 1998 in der heutigen Form aus einer Fusion zwischen verschiedenen kleineren Parteien und der ADF mit dem 
+    traditionsreichen Rassemblement Démocratique Africain, <span style="background-color: red;">RDA</span> hervorgegangen, in dessen programmatischer Tradition sie 
+    sich bis heute sieht. Die ADF-RDA ist von der Wählerstärke her betrachtet eine der konstantesten Parteien Burkina Fasos. Bereits an den Parlamentswahlen vom 24. 
+    Mai 1992 und 11. Mai 1997, welche von der ADF und dem RDA noch unabhängig voneinander bestritten wurden, kamen sie zusammen auf einen ähnlichen Wähleranteil von 
+    rund 13 %.""")
+
+        text_points = ui.markdown("10 points remaining")
+
+        with ui.row() as row:
+            text_paranoid = ui.markdown("paranoid")
+            element_diagram = ui.element()
+            text_gullible = ui.markdown("gullible")
+
+        with ui.row() as row:
+            button_bot = ui.button("BOT")
+            button_human = ui.button("HUMAN")
+
+    create_footer()
+
+
+@ui.page("/aces")
+def aces_page() -> None:
+    with ui.header(elevated=True):
+        link_home = ui.link("home", "/")
+        label_title = ui.label("Top Aces")
+
+    for i in range(10):
+        ui.label(f"[ace {i + 1}] [accuracy]")
+
+    create_footer()
+
+
+@ui.page("/results")
+def results_page() -> None:
+    with ui.header(elevated=True):
+        link_home = ui.link("home", "/")
+        label_title = ui.label("Results")
+
+    for i in range(10):
+        ui.label(f"[result {i + 1}] [accuracy]")
+
+    text_example = ui.markdown("""Die ADF-RDA ist 1998 in der heutigen Form aus einer Fusion zwischen verschiedenen kleineren Parteien und der ADF mit dem 
+    traditionsreichen Rassemblement Démocratique Africain, <span style="background-color: red;">RDA</span> hervorgegangen, in dessen programmatischer Tradition sie 
+    sich bis heute sieht. Die ADF-RDA ist von der Wählerstärke her betrachtet eine der konstantesten Parteien Burkina Fasos. Bereits an den Parlamentswahlen vom 24. 
+    Mai 1992 und 11. Mai 1997, welche von der ADF und dem RDA noch unabhängig voneinander bestritten wurden, kamen sie zusammen auf einen ähnlichen Wähleranteil von 
+    rund 13 %.""")
+
+    create_footer()
+
+
+@ui.page("/about")
+def about_page() -> None:
+    with ui.header(elevated=True):
+        link_home = ui.link("home", "/")
+        label_title = ui.label("About")
+
+    text_example = ui.markdown("bla bla bla")
+
+    create_footer()
 
 
 def main() -> None:
