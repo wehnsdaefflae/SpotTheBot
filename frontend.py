0a5488d8b78241b348eb0b1dcc650760772e7954
mark <wernsdorfer@gmail.com>
2023-09-26 13:08:01 +0200

    added tagging

--- frontend.py
+++ frontend.py
@@ -2,11 +2,31 @@
 import dataclasses
 import random
 import re
+from typing import Callable, Coroutine
 
 from fastapi import FastAPI
 from nicegui import ui, app
-from nicegui.elements.dialog import Dialog
 from nicegui.elements.label import Label
+from nicegui.events import ClickEventArguments
+
+
+async def increment_tagged_word_count() -> None:
+    await ui.run_javascript(
+        f"window.tagged_word_count++; ", respond=False)
+
+    await ui.run_javascript(
+        f"window.submit_button.children[1].children[0].innerText = 'It is a bot!';", respond=False
+    )
+
+
+async def decrement_tagged_word_count() -> None:
+    await ui.run_javascript("window.tagged_word_count = Math.max(tagged_word_count - 1, 0);", respond=False)
+    await ui.run_javascript(
+        "if (window.tagged_word_count < 1) {\n"
+        "    window.submit_button.children[1].children[0].innerText = 'I am sure it is fine...';\n"
+        "}", respond=False
+    )
+
 
 
 def get_random_name() -> str:
@@ -81,14 +101,12 @@ class Snippet:
 
 def next_snippet(user_name: str) -> Snippet:
     content = (
-        f"""
-        TEXT {random.randint(0, 100)}:
-        
-        Die ADF-RDA ist 1998 in der heutigen Form aus einer Fusion zwischen verschiedenen kleineren Parteien und der ADF mit dem traditionsreichen Rassemblement 
-        Démocratique Africain, <span style=\"background-color: red;\">RDA</span> hervorgegangen, in dessen programmatischer Tradition sie sich bis heute sieht. Die 
-        ADF-RDA ist von der Wählerstärke her betrachtet eine der konstantesten Parteien Burkina Fasos. Bereits an den Parlamentswahlen vom 24. Mai 1992 und 11. Mai 
-        1997, welche von der ADF und dem RDA noch unabhängig voneinander bestritten wurden, kamen sie zusammen auf einen ähnlichen Wähleranteil von rund 13 %.
-        """
+        f"TEXT {random.randint(0, 100)}:\n"
+        f"\n"
+        f"Die ADF-RDA ist 1998 in der heutigen Form aus einer Fusion zwischen verschiedenen kleineren Parteien und der ADF mit dem traditionsreichen Rassemblement "
+        f"Démocratique Africain, RDA hervorgegangen, in dessen programmatischer Tradition sie sich bis heute sieht. Die ADF-RDA ist von der Wählerstärke her betrachtet "
+        f"eine der konstantesten Parteien Burkina Fasos. Bereits an den Parlamentswahlen vom 24. Mai 1992 und 11. Mai 1997, welche von der ADF und dem RDA noch "
+        f"unabhängig voneinander bestritten wurden, kamen sie zusammen auf einen ähnlichen Wähleranteil von rund 13 %."
     )
     return Snippet(
         source="Wikipedia",
@@ -96,50 +114,89 @@ def next_snippet(user_name: str) -> Snippet:
     )
 
 
-def assumed_human(user_name: str, snippet: Snippet, points: int) -> None:
+def submit(user_name: str, snippet: Snippet, points: int) -> None:
     print(f"{user_name} assumed {hash(snippet)} as HUMAN with {points} points")
+    global tagged_word_count, button_text
+    tagged_word_count = 0
+    button_text = "I'm sure it's fine..."
+
     # update stats
     #   if is_bot:
     #       register as gullible
     ui.open("/game")
 
 
-def assumed_bot(user_name: str, snippet: Snippet, points: int) -> None:
-    print(f"{user_name} assumed {hash(snippet)} as BOT with {points} points")
-
-    def update_stats(_dialog: Dialog) -> None:
-        # update stats
-        #   if is_bot:
-        #       revert paranoid
-
-        _dialog.close()
-        ui.open("/game")
-
-    with ui.dialog() as dialog, ui.card():
-        # register as paranoid
-        label_title = ui.label("What made you suspicious?")
-        label_title.classes("text-h4 font-bold text-grey-8")
-        select_text(snippet)
-        button_close = ui.button("Close", on_click=lambda: update_stats(dialog))
-        button_close.disable()
-
-    dialog.props("persistent")
-
-    dialog.open()
+def get_signs() -> list[str]:
+    return [
+        "ausweichend",
+        "zu allgemein",
+        "abgedroschen",
+        "unlogisch",
+        "unverständlich",
+        "künstlich",
+        "monoton",
+        "redundant",
+        "langweilig",
+        "unpräzise",
+        "stereotyp",
+        "formelhaft",
+        "gekünstelt",
+        "unspezifisch",
+        "wiederholt",
+        "unzusammenhängend"
+    ]
 
 
-def display_snippet(snippet: Snippet) -> None:
-    text_context = ui.markdown(f"SOURCE: {snippet.source}")
-    text_content = ui.markdown(snippet.content)
+def get_tagging(label_word: ui.label) -> Callable[[ClickEventArguments], Coroutine[None, None, None]]:
+    async def _tag_word(event: ClickEventArguments) -> None:
+        print(label_word.text + " ist zu " + event.sender.text)
+        label_word.style(add="background-color: red;")
+        label_word.sus_sign = event.sender.text
+        await increment_tagged_word_count()
+
+    return _tag_word
 
 
-def select_text(snippet: Snippet) -> None:
-    with ui.row() as text_content_clickable:
-        for each_word in re.split(" ", snippet.content):
-            label_word = ui.label(each_word)
-            label_word.classes("cursor-pointer")
-            label_word.on("click", lambda event: print(event.sender.text))
-            # label_word
+async def click_event(event: ClickEventArguments) -> None:
+    word_label = event.sender
+    assert isinstance(word_label, ui.label)
+    if word_label.sus_sign is None:
+        signs = get_signs()
+        tag_word = get_tagging(word_label)
+        with ui.menu() as menu:
+            for each_sign in signs:
+                ui.menu_item(each_sign, on_click=tag_word)
+            ui.separator()
+            ui.menu_item("something else...", on_click=lambda: None)
+
+        menu.move(word_label)
+        menu.open()
+
+    else:
+        await decrement_tagged_word_count()
+        word_label.default_slot.children.clear()
+        word_label.style(remove="background-color: red;")
+        word_label.sus_sign = None
+
+
+def show_text(snippet: Snippet) -> ui.column:
+    lines = snippet.content.split("\n")
+
+    with ui.column() as text_content:
+        for line_number, each_line in enumerate(lines):
+            with ui.row() as text_content_clickable:
+                for each_word in re.split(" ", each_line):
+                    if len(each_word) < 1:
+                        continue
+
+                    each_word = each_word.strip()
+                    label_word = ui.label(each_word)
+                    label_word.on("click", lambda event: click_event(event))
+
+                    label_word.sus_sign = None
+                    label_word.classes("cursor-pointer")
+
+    return text_content
 
 
 @ui.page("/game")
@@ -156,10 +213,10 @@ def game_page() -> None:
     word_counter = 0
     points = 100
     snippet = next_snippet(user_name)
-    # words = snippet.content.split(" ")
 
     with ui.column() as column:
-        display_snippet(snippet)
+        text_context = ui.markdown(f"SOURCE: {snippet.source}")
+        text_content = show_text(snippet)
 
         text_points = ui.markdown(f"{points} points remaining")
 
@@ -169,10 +226,15 @@ def game_page() -> None:
             element_diagram = ui.element()
             text_gullible = ui.markdown("gullible")
 
-        with ui.row() as row:
-            button_bot = ui.button("BOT", on_click=lambda: assumed_bot(user_name, snippet, points))
-            button_human = ui.button("HUMAN", on_click=lambda: assumed_human(user_name, snippet, points))
+        submit_button = ui.button("<empty>", on_click=lambda: submit(user_name, snippet, points))
+        submit_button.classes("w-full justify-center")
 
+    async def init_tag_count() -> None:
+        await ui.run_javascript("window.tagged_word_count = 0;", respond=False)
+        await ui.run_javascript(f"window.submit_button = document.getElementById('c{submit_button.id}');", respond=False)
+        await decrement_tagged_word_count()
+
+    app.on_connect(init_tag_count)
     create_footer()
 
     def increment_counter() -> None:
