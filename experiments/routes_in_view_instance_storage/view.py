cdfe622fbb95bab68daabad028d55fb48a448669
Mark <wernsdorfer@gmail.com>
2023-10-17 20:26:10 +0200

    view instance is for all clients

--- experiments/routes_in_view_instance_storage/view.py
+++ experiments/routes_in_view_instance_storage/view.py
@@ -3,10 +3,13 @@ from nicegui import ui, app
 
 class View:
     def __init__(self):
+        self.d = {"text_input": "initial value"}
         self.create_routes()
 
     def create_routes(self) -> None:
         @ui.page("/")
         def main_page() -> None:
-            name = app.storage.user.get("name", "default")
-            ui.label(f"Hello {name}!")
+            label = ui.label()
+            label.bind_text_from(self.d, "text_input")
+            text_input = ui.input("tell me")
+            text_input.on("change", lambda: self.d.__setitem__("text_input", text_input.value))
