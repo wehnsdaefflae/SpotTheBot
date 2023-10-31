a1a1b047cba06232fa4093e962207df9eafe58be
Mark <wernsdorfer@gmail.com>
2023-10-31 13:46:29 +0100

    fixed shell

--- start_databases.sh
+++ start_databases.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-redis-server databases/general.conf
+redis-server databases/general.conf &
 
 # Wait for a moment to ensure the first instance starts without issues
 sleep 2
 
-redis-server databases/markers.conf
+redis-server databases/markers.conf &
 
 echo "Both Redis instances have been started."
