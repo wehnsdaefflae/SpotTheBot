cc3ca393d110eb59fbb99b3945332b00959534f8
Mark <wernsdorfer@gmail.com>
2023-10-31 15:29:35 +0100

    changes model interfaces to databases

--- src/main.py
+++ src/main.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 import json
-import webbrowser
+
 from nicegui import ui
 
 from src.controller import Controller
 
 
-# redis-server database/redis.conf
-
 def main() -> None:
     c = Controller()
 
-    with open("config.json", mode="r") as config_file:
+    with open("../config.json", mode="r") as config_file:
         config = json.load(config_file)
 
     nicegui_config = config["nicegui"]
-    # webbrowser.open(f"http://{address}:{port}")
 
     ui.run(**nicegui_config)
 
