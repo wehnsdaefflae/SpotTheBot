8d2227e18d481cc728ae425ac249e31bf366394a
Mark <wernsdorfer@gmail.com>
2023-10-31 13:45:03 +0100

    added shell script to start databases

---
+++ start_databases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+redis-server databases/general.conf
+
+# Wait for a moment to ensure the first instance starts without issues
+sleep 2
+
+redis-server databases/markers.conf
+
+echo "Both Redis instances have been started."
